e750f1638209c32087836d843964d91a2786342b
drebo <drebo2324@gmail.com>
2024-10-03 18:32:36 -0400

    HttpServiceProxy

--- src/main/java/com/drebo/microservices/order/config/RestClientConfig.java
+++ src/main/java/com/drebo/microservices/order/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.drebo.microservices.order.config;
 
 import com.drebo.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,11 +23,25 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                //RestClient internally uses ClientHttpRequestFactory to handle mechanics of http calls
+                //ClientHttpRequestFactory creates instances of ClientHttpRequest -> http request object that RestClient uses to make http calls
+                .requestFactory(getClientRequestFactory())
                 .build();
 
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        //creates proxy instance for InventoryClient
+        //proxy translates method calls using http requests via RestClient
         return httpServiceProxyFactory.createClient(InventoryClient.class);
 
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory(){
+        //settings for ClientHttpRequest used by the RestClient
+        //set timeout
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
